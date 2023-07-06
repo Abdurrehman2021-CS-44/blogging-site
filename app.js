ac28a9c303ec197c91449ebf63ba6d95526e23e5
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-06 16:49:38 +0500

    Connect the app to the local database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,6 +13,35 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const posts = [];
 
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/postsDB")
+.then(()=>{
+  console.log("Connected");
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true,"Please add post title"]
+  },
+  body: {
+    type: String,
+    required: [true,"Please add post body"]
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post = new Post({
+  title: "Animals",
+  body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui." 
+});
+
+post.save()
+.then(()=>{
+  console.log("Post is added");
+  mongoose.connection.close()
+});
 
 app.set('view engine', 'ejs');
 
