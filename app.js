35e231e8621776ce31ef1d1ceb69897cc0af41ca
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-06 16:55:16 +0500

    View posts from the database on the root route

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/postsDB")
@@ -32,16 +30,16 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model("Post", postSchema);
 
-const post = new Post({
-  title: "Animals",
-  body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui." 
-});
+// const post = new Post({
+//   title: "Animals",
+//   body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui." 
+// });
 
-post.save()
-.then(()=>{
-  console.log("Post is added");
-  mongoose.connection.close()
-});
+// post.save()
+// .then(()=>{
+//   console.log("Post is added");
+//   mongoose.connection.close()
+// });
 
 app.set('view engine', 'ejs');
 
@@ -49,7 +47,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent, allPosts: posts});
+  Post.find({})
+  .then((list)=>{
+    res.render("home", {startingContent: homeStartingContent, allPosts: list});
+  });
 });
 
 app.get("/contact", function(req, res){
@@ -74,6 +75,12 @@ app.get("/posts/:postName", function(req, res){
 });
 
 app.post("/compose", function(req, res){
+  const postTitle = req.body.postTitle;
+  const postBody = req.body.postBody;
+  const postDB = new Post({
+    title: postTitle,
+    body: postBody 
+  });
   const post = {
     title: req.body.postTitle,
     body: req.body.postBody
