06f4aec2163abf4745fda0e6ef899d6cb3e52c22
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-08-10 23:43:16 +0500

    Change footer position from static to fixed

--- app.js
+++ app.js
@@ -30,17 +30,6 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model("Post", postSchema);
 
-// const post = new Post({
-//   title: "Animals",
-//   body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui." 
-// });
-
-// post.save()
-// .then(()=>{
-//   console.log("Post is added");
-//   mongoose.connection.close()
-// });
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -90,15 +79,6 @@ app.post("/compose", function(req, res){
   });
   res.redirect("/");
 });
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
